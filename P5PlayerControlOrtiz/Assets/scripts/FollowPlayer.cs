ea8839b16c8c3dffee66fe1ca4f383cf7609fc8d
AntO-ON <723529@student.dusd.net>
2024-03-06 13:46:19 -0800

    DO AT HOME

--- P5PlayerControlOrtiz/Assets/scripts/FollowPlayer.cs
+++ P5PlayerControlOrtiz/Assets/scripts/FollowPlayer.cs
@@ -8,7 +8,7 @@ public class FollowPlayer : MonoBehaviour
     private Vector3 offset = new Vector3(0, 5, -7);
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
